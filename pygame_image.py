0ba694db6c486a008b2a9128a2a62fec59bf426a
c0a22138cc <c0a22138cc@edu.teu.ac.jp>
2023-04-18 16:05:48 +0900

    練習問題5

--- pygame_image.py
+++ pygame_image.py
@@ -9,6 +9,8 @@ def main():
     kt_img = pg.image.load("ex01/fig/3.png")
     kt_img_1 = pg.transform.flip(kt_img, True, False)
     kt_img_2 = pg.transform.rotozoom(kt_img_1,10,1.0)
+    kt_list = [kt_img_1,kt_img_2]
+    x = 0
 
     tmr = 0
 
@@ -17,11 +19,13 @@ def main():
             if event.type == pg.QUIT: return
 
         tmr += 1
+        x -=1
         screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [1600,0])
         if tmr%2==0:
-            screen.blit(kt_img_1,[300,200])
+            screen.blit(kt_list[0],[300,200])
         else:
-            screen.blit(kt_img_2,[300,200])
+            screen.blit(kt_list[1],[300,200])
 
         pg.display.update()
         clock.tick(100)
